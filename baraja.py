302dd9f3a54f6b5a67c7884c11c2c1b3ea77dee1
PedroMoreno2 <130315573+PedroMoreno2@users.noreply.github.com>
2023-05-09 13:16:54 +0200

    Update baraja.py

--- baraja.py
+++ baraja.py
@@ -1,24 +1,15 @@
 import streamlit as st
-st.header("La vida en el mar")
-listilla=["sardina","mero","tiburón ballena","pulpo"]
-fotillos=["F1","F2","F3"]
-elec_animal=st.radio("Elige el animal que más te gusta:",listilla)
-if elec_animal==listilla[0]:
-    st.error("por el monte la sardina")
-elif elec_animal==listilla[1]:
-    st.warning("uno mero dos de febrero")
-elif elec_animal==listilla[2]:
-    st.success("por fin llegamos a lo que interesa")
-    st.image("https://www.bekaretransfers.com/blog/wp-content/uploads/2017/05/whaleshark.jpeg")
-elif elec_animal==listilla[3]:
-    st.text("elige tiburón ballena por favor")
-if(st.button("Click me for no reason")):
+import requests
+baraja=requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
+baraja_id=
+
+
+st.header("Baraja, cartas al azar")
+
+
+if(st.button("Carta al azar")):
     st.markdown(":red[lo estás haciendo muy bien]")
-elec_foto=st.selectbox("Ahora lo complicado:",
-                     ["Elige una foto"]+fotillos)
-if elec_foto=="F1":
-    st.image("F1.jpg",width=500)
-elif elec_foto=="F2":
-    st.image("F2.jpg",width=500)
-elif elec_foto=="F3":
-    st.image("F3.jpg")
+
+
+https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2
+
