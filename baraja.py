3085e12f3e9cbfdf0df704ffadc2e2c535ecab3b
PedroMoreno2 <130315573+PedroMoreno2@users.noreply.github.com>
2023-05-11 10:51:32 +0200

    Update baraja.py

--- baraja.py
+++ baraja.py
@@ -1,15 +1,11 @@
 import streamlit as st
 import requests
 baraja=requests.get("https://deckofcardsapi.com/api/deck/new/shuffle/?deck_count=1")
-baraja_id=
-
-
+codigo=baraja.json()["deck_id"]
+# Ahora a sacar una carta de esa baraja:
+cartas=requests.get("https://deckofcardsapi.com/api/deck/"+codigo+"/draw/?count=1")
+carta_d=cartas.json()["cards"]
+carta_image=carta_d[0]["image"]
 st.header("Baraja, cartas al azar")
-
-
 if(st.button("Carta al azar")):
-    st.markdown(":red[lo estás haciendo muy bien]")
-
-
-https://deckofcardsapi.com/api/deck/<<deck_id>>/draw/?count=2
-
+    st.image("carta_image")
